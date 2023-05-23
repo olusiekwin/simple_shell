55d2b08fb045f96cb7cdb86081e81c53cf26a833
olusiekwin <olusiekwingraham@gmail.com>
2023-05-24 00:08:32 +0300

    prompt

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+char *getline_custom(void);
+void execute_command(char *command);
+void handle_builtin_commands(char *command, int *exit_code);
+void handle_exit_command(char *command, int *exit_code);
+void handle_cd_command(char *command);
+void handle_setenv_command(char *command);
+void handle_unsetenv_command(char *command);
+
+#endif /* SHELL_H */
---
+++ util.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * getline_custom - Reads a line of input from stdin
+ *
+ * Return: Pointer to the line read (including newline character)
+ *         or NULL if EOF is reached
+ */
+char *getline_custom(void) {
+  char *line = NULL;
+  size_t bufsize = 0;
+  ssize_t n;
+
+  n = getline(&line, &bufsize, stdin);
+
+  if (n == -1) {
+    if (feof(stdin)) {
+      return NULL;
+    } else {
+      perror("getline");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return line;
+}
